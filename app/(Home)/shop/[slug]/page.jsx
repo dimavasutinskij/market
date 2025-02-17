647aba2309205f68e5c58230ea4bca94ae09831f
dimavasutinskij <dimavasutinskij45@gmail.com>
2025-02-17 19:46:26 +0100

    qqq5655364

--- app/(Home)/shop/[slug]/page.jsx
+++ app/(Home)/shop/[slug]/page.jsx
@@ -4,41 +4,51 @@ import ProductDetails from "../_components/ProductDetails";
 import ProductInfo from "../_components/ProductInfo";
 import RelatedProducts from "../_components/RelatedProducts";
 
-
-export const revalidate = 90
+// Оновлення параметрів для статичної генерації
+export const revalidate = 90;
 
 export async function generateStaticParams() {
-  
-  const count = await getProductsCount()
+    // Отримуємо кількість продуктів
+    const count = await getProductsCount();
 
-  const products = await getProducts(0, count)
+    // Отримуємо всі продукти
+    const products = await getProducts(0, count);
 
-  return products.map((product) => ({
-    slug: product.id,
-    count: 0
-  }))
+    // Повертаємо масив параметрів для кожного продукту
+    return products.map((product) => ({
+        slug: [product.slug.current], // Масив для використання в динамічному маршруті
+    }));
 }
 
+// Головна сторінка продукту
+const ProductPage = async ({ params: { slug } }) => {
+    // Отримуємо кількість продуктів
+    const count = await getProductsCount();
 
+    // Отримуємо продукти
+    const products = await getProducts(0, count);
 
-const ProductPgae = async ({ params: { slug } }) => {
+    // Знаходимо продукт, що відповідає поточному `slug`
+    const product = products.find((product) => product.slug.current === slug);
 
-  const count = await getProductsCount()
-  const products = await getProducts(0, count)
+    // Отримуємо схожі продукти, що належать до тієї ж категорії
+    const related = products.filter((item) => item.category === product.category).slice(0, 4);
 
-  const product = products.find(product => product.slug.current === slug)
+    return (
+        <main className="overflow-hidden">
+            {/* Виводимо банер для сторінки продукту */}
+            <Banner bgUrl="/productPage.png" title="Product's Details" />
 
-  const related = products.filter(item => item.category === product.category).slice(0, 4)
+            {/* Виводимо деталі продукту */}
+            <ProductDetails product={product} />
 
+            {/* Інформація про продукт */}
+            <ProductInfo product={product} />
 
-  return ( 
-    <main className="overflow-hidden">
-      <Banner bgUrl="/productPage.png" title="Product's Details" />
-      <ProductDetails product={product} />
-      <ProductInfo product={product} />
-      <RelatedProducts related={related} />
-    </main>
-  );
-}
+            {/* Виводимо схожі продукти */}
+            <RelatedProducts related={related} />
+        </main>
+    );
+};
 
-export default ProductPgae;
+export default ProductPage;
