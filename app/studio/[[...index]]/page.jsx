aa7db9b89cdac55ddee3a6cd15251280362e8760
dimavasutinskij <dimavasutinskij45@gmail.com>
2025-02-17 17:46:08 +0100

    second try

--- app/studio/[[...index]]/page.jsx
+++ app/studio/[[...index]]/page.jsx
@@ -1,16 +1,17 @@
 'use client'
 
-/**
- * This route is responsible for the built-in authoring environment using Sanity Studio.
- * All routes under your studio path is handled by this file using Next.js' catch-all routes:
- * https://nextjs.org/docs/routing/dynamic-routes#catch-all-routes
- *
- * You can learn more about the next-sanity package here:
- * https://github.com/sanity-io/next-sanity
- */
-
 import { NextStudio } from 'next-sanity/studio'
 import config from '../../../sanity.config'
+
+// Додай generateStaticParams для динамічних параметрів
+export async function generateStaticParams() {
+  // Якщо у тебе є список динамічних параметрів для шляху, додай їх тут
+  return [
+    { index: 'page-1' },  // Приклад статичних параметрів
+    { index: 'page-2' },  // Замінити на реальні значення
+    { index: 'page-3' }   // Якщо це має бути динамічним
+  ]
+}
 
 export default function StudioPage() {
   return <NextStudio config={config} />
