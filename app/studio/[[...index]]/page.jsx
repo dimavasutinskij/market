dff9e62a65bcf5f8650e12d5322cd21383b847fa
dimavasutinskij <dimavasutinskij45@gmail.com>
2025-02-17 19:01:44 +0100

    qqq5655

--- app/studio/[[...index]]/page.jsx
+++ app/studio/[[...index]]/page.jsx
@@ -1,13 +1,18 @@
-// pages/studio/[[...index]].jsx
-
+// Генерація статичних параметрів для маршруту
 export async function generateStaticParams() {
   return [
     { index: 'page-1' },
     { index: 'page-2' },
-    { index: 'page-3' }
+    { index: 'page-3' },
   ];
 }
 
-export default function StudioPage() {
-  return <div>Це сторінка зі статичними параметрами.</div>;
+// Сторінка Studio
+export default function StudioPage({ params }) {
+  return (
+      <div>
+        Це сторінка для:
+        {params.index ? params.index : 'без параметрів'}
+      </div>
+  );
 }
